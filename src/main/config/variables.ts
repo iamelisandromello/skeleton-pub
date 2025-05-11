c5e8bf38767820e769a3e67207ebea4b24294bdc
Elisandro Mello Correa <elisandro@ulbra.edu.br>
2025-05-11 00:15:45 -0300

    refactor: update environments variables

--- src/main/config/variables.ts
+++ src/main/config/variables.ts
@@ -2,6 +2,5 @@ import env from 'env-var'
 
 export const variables = {
   corsOriginPermission: env.get('CORS_ORIGIN_PERMISSION').required().asString(),
-  timezone: env.get('TZ').required().asString(),
   queueSQS: env.get('EXAMPLE_QUEUE_URL').required().asString()
 }
