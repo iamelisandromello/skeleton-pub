0df4fb8f5535537e10d3709e8c5373c3e7bf133f
Elisandro Mello Correa <elisandro@ulbra.edu.br>
2025-05-12 13:40:35 -0300

    refactor: use lambda_env_vars object in pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 # Usado para armazenar o artefato zip da função Lambda.
 # Sempre reutilizado/importado. Não deve ser gerenciado pela criação.
 data "aws_s3_bucket" "lambda_code_bucket" {
-  bucket = var.s3_bucket_name
+  bucket = local.s3_bucket_name
 }
 
 # =======================================
