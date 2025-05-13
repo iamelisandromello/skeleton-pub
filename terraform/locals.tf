90be8570b64698ea1cd2da0cb590409489e487b8
Elisandro Mello Correa <elisandro@ulbra.edu.br>
2025-05-13 10:24:21 -0300

    refactor: add lifecycle strategy in terraform import resource

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,16 +1,16 @@
 locals {
   # Identificadores padronizados
-  lambda_name          = "${var.project_name}${local.environment_suffix}"
-  lambda_role_name     = "${var.project_name}_execution_role"
-  logging_policy_name  = "${var.project_name}_logging_policy"
-  publish_policy_name  = "${var.project_name}-lambda-sqs-publish"
   environment_suffix   = var.environment == "prod" ? "" : "-${var.environment}"
 
+  lambda_name          = "${var.project_name}${local.environment_suffix}"
+  lambda_role_name     = "${var.project_name}${local.environment_suffix}_execution_role"
+  logging_policy_name  = "${var.project_name}${local.environment_suffix}_logging_policy"
+  publish_policy_name  = "${var.project_name}${local.environment_suffix}-lambda-sqs-publish"
 
   # Infraestrutura associada
-  s3_bucket_name       = var.s3_bucket_name
   log_group_name       = "/aws/lambda/${local.lambda_name}"
-
-  s3_object_key        = "${var.project_name}.zip"  
   queue_name           = "${local.lambda_name}-queue"
+
+  s3_bucket_name       = var.s3_bucket_name
+  s3_object_key        = "${var.project_name}.zip"
 }
